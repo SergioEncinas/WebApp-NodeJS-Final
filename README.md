311d7dbc95bb98aac0ea6b6c04ca9f9f675384f6
Sergio Encinas <74409211+SergioEncinas@users.noreply.github.com>
2024-02-25 19:43:58 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# WebApp NodeJS + MySQL
+- Esta es una aplicación básica que utiliza NodeJS en el backend y MySql como BBDD
+- Para el front se ha utilizado HTML, CSS y Bootstrap 4
+
+# Usefull Commands
+- to init mysql: `mysql -u root -p`
---
+++ database/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE javafinaldb;
+
+use javafinaldb;
+
+CREATE TABLE customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+
+
+select * from javafinaldb;
